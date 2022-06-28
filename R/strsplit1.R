3ec3858d37b65aa4e51cd8b07890b61eedbd3d5c
Juan Vera <juan.vera.f@protonmail.com>
2022-06-27 23:02:01 -0400

    Modificaciones al introducir documentación con roxygen

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,14 @@
+#' Divide una cadena de texto
+#'
+#' @param x Un vector de caracter con un elemento.
+#' @param split Cómo dividir el elemento del vector.
+#'
+#' @return Un vector de caracter
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
